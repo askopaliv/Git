35906344639bd21cd7202c8ecb9c23c47745d4fb
yosef <yosefbuganim500@GMAIL.COM>
2025-06-17 00:10:04 +0300

    final bin

--- bin.py
+++ bin.py
@@ -8,12 +8,12 @@ def toNegative(num):
         if(num >= checking):
             list[i] = 0
             num -= checking
-        checking = math.sqrt(checking)
+        checking = math.pow(2, 7-i)
     
    for i in range(len(list)):
-       if(list[i] == 0 & makeit[i] ==1 | list[i] == 1 & makeIt[i] == 0):
+       if(list[i] == 0 and makeit[i] ==1 or list[i] == 1 and makeIt[i] == 0):
            list[i] = 1
-       elif(list[i] == 1 & makeIt[i] == 1):
+       elif(list[i] == 1 and makeIt[i] == 1):
            list[i] = 0
            num = 1
        else:
